0a8495bc2072f1507b1438fca586873a97988571
Thomas <thomsult@gmail.com>
2023-04-15 01:27:28 +0200

    add getUserBy Email

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,8 +1,14 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
 import { UnauthorizedException } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 
 export class AuthGuard implements CanActivate {
+  public token: jwt;
   async canActivate(ctx: ExecutionContext) {
     const VerifyToken = (token: string) => {
       // do something async
@@ -20,7 +26,7 @@ export class AuthGuard implements CanActivate {
       const isAuthenticated = await new Promise<jwt>((resolve) => {
         const auth = req.headers.authorization.split(' ')[1];
         const token = VerifyToken(auth) as jwt;
-        console.log(token);
+        this.token = token;
         resolve(token);
       });
       req.user = isAuthenticated;
@@ -30,5 +36,17 @@ export class AuthGuard implements CanActivate {
       });
     }
     return true;
+  }
+}
+
+@Injectable()
+export class TokenEmail implements PipeTransform {
+  constructor(private authGuard: AuthGuard) {}
+  transform(value: string) {
+    if (value === undefined && this.authGuard.token.email) {
+      const { email } = this.authGuard.token;
+      return email;
+    }
+    return value;
   }
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,20 +1,46 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { Users } from './schema/users.schema';
-import { AuthGuard } from '../auth/auth.guard';
+import { AuthGuard, TokenEmail } from '../auth/auth.guard';
 import { UseGuards } from '@nestjs/common';
 import { Context } from 'vm';
+import { NotFoundException } from '@nestjs/common';
 
-
+const authGuard = new AuthGuard();
 @Resolver(() => Users)
-@UseGuards(AuthGuard)
+@UseGuards(authGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [Users], { name: 'ListAllUsers' })
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  findAll(obj, args, context: Context, info): Promise<Users[]> {
-    console.log(context.req.user);
-    return this.usersService.findAll();
+  async findAll(obj, args, context: Context, info): Promise<Users[]> {
+    return await this.usersService.findAll();
+  }
+  @Query(() => Users, { name: 'getUserBy' })
+  async findOne(
+    @Args(
+      'email',
+      {
+        nullable: true,
+        description: 'Email of the user',
+      },
+      new TokenEmail(authGuard),
+    )
+    email?: string,
+  ): Promise<Users> {
+    try {
+      const user = await this.usersService.findOneByEmail({
+        email: email,
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user;
+    } catch (error) {
+      throw new NotFoundException({
+        message: error.message,
+      });
+    }
   }
 }
